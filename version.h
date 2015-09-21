8f775f1426c3e1fd6080bdc4d41a9913443bd86e
Igrek <igrek.s@o2.pl>
2015-09-21 12:19:49 +0200

    dupa

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
-// -- 15:55:23, 04.09.2015 -- (ymake v1.6.1) --
+// -- 12:15:42, 21.09.2015 -- (ymake v1.6.5) --
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "1.5.10"
+#define VERSION "1.5.12"
 
 #endif
